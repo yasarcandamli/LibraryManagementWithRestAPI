e3f60c322556e4f2ad548658a74359a8526ca438
yasarcandamli <yasarcandamli@gmail.com>
2024-07-12 22:10:17 +0300

    Initial Commit

---
+++ src/main/java/dev/patika/LibraryManagementWithAPI/LibraryManagementWithApiApplication.java
@@ -0,0 +1,13 @@
+package dev.patika.LibraryManagementWithAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementWithApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementWithApiApplication.class, args);
+	}
+
+}
